6e59b61eff6bf98561eff951a1fc0775840b0da8
pablodmv@gmail.com <pablodmv@gmail.com@a257efec-d99e-9a0d-1fb1-26c13d0284a1>
2011-06-15 01:40:27 +0000

    renombrado paquete objVirtual a vo

---
+++ trunk/common/src/vo/PersistenceObj.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vo;
+
+/**
+ *
+ * @author Gustavo Leites
+ */
+public class PersistenceObj {
+
+    private boolean persistent;
+
+    public PersistenceObj(){
+
+    }
+
+    public boolean isPersistent() {
+        return persistent;
+    }
+
+    public void setPersistent(boolean persistent) {
+        this.persistent = persistent;
+    }
+
+    
+
+
+}
